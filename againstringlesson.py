2262b6a1cb2a430ddb090623db7c46e0b8be4abf
Nurba2405 <nurbekorazov@gmail.com>
2024-12-02 17:40:43 +0500

    После долгой паузы, отправляю в гитхаб

---
+++ againstringlesson.py
@@ -0,0 +1,31 @@
+'''s1 = 'hello'
+number = 741
+s2 = str(number)
+print(type(s1))
+print(type(s2))'''
+
+'''s1 = 'hello'
+s2 = 'world'
+s3 = (s1+s2) * 10
+print(s3)'''
+
+'''s1 = 'hello'
+s2 = 'world'
+s3 = (s1+s2) * 10
+if s1 in s3:
+    print(True)
+else:
+    print(False)'''
+
+''' # Поиск символа в строках
+s1 = 'a'
+if s1 in 'bcsd':
+    print(True)
+else:
+    print(False)'''
+
+'''#развернуть строку
+s1 = 'inform'
+print(s1[::-1])'''
+
+
